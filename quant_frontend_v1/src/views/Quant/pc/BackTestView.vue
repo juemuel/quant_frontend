88e241ec1d1b133c9c35a823d635fef355855071
Juemuel <799525644@qq.com>
2024-09-26 19:20:24 +0800

    Update BackTestView.vue

--- quant_frontend_v1/src/views/Quant/pc/BackTestView.vue
+++ quant_frontend_v1/src/views/Quant/pc/BackTestView.vue
@@ -77,15 +77,91 @@
         <!-- 收益年分布图 -->
       </el-tab-pane>
       <el-tab-pane label="收益年分布表" name="forth">
-        <!-- 收益年分布表 -->
+        <div class="label label-warning">收益年分布表</div>
+        <table class="table table-striped table-bordered table-condensed">
+          <thead>
+            <th>年份</th>
+            <th>指数收益</th>
+            <th>趋势投资收益</th>
+          </thead>
+          <tbody>
+            <!-- <tr v-for="bean in data.annualProfits">
+              <td>
+                {{ bean.year }}
+              </td>
+              <td>
+                {{ (bean.indexIncome * 100).toFixed(2)}}%
+              </td>
+              <td>
+                {{ (bean.trendIncome * 100).toFixed(2)}}%
+              </td>
+            </tr> -->
+          </tbody>
+        </table>
       </el-tab-pane>
     </el-tabs>
     <el-tabs v-model="data.activeProfitTab" @tab-click="handleProfitClick">
-      <el-tab-pane label="交易小结" name="first">
-        <!-- 交易小结表格 -->
+      <el-tab-pane label="策略交易统计" name="first">
+        <el-table :data="data.tradeStatistics" stripe style="width: 100%">
+          <el-table-column prop="totalTradeCount" label="总共交易次数" width="200" align="center"></el-table-column>
+          <el-table-column prop="winCount" label="盈利交易次数" width="200" align="center"></el-table-column>
+          <el-table-column prop="avgWinRate" label="平均盈利比率" width="200" align="center">
+            <template #default="scope">
+              {{ (scope.row.avgWinRate * 100).toFixed(2)}}%
+            </template>
+          </el-table-column>
+          <el-table-column prop="lossCount" label="亏损交易次数" width="200" align="center">
+          </el-table-column>
+          <el-table-column prop="avgLossRate" label="平均亏损比率" width="200" align="center">
+            <template #default="scope">
+              {{ (scope.row.avgLossRate * 100).toFixed(2)}}%
+            </template>
+          </el-table-column>
+          <el-table-column prop="winRate" label="胜率" width="200" align="center">
+            <template #default="scope">
+              {{ (scope.row.winRate * 100).toFixed(2)}}%
+            </template>
+          </el-table-column>
+        </el-table>
       </el-tab-pane>
       <el-tab-pane label="交易明细表" name="second">
-        <!-- 交易明细表 -->
+        <table class="table table-striped table-bordered table-condensed table-hover">
+          <!-- <thead>
+            <th>盈/亏</th>
+            <th>购买日期</th>
+            <th>购买盘点</th>
+            <th>出售日期</th>
+            <th>出售盘点</th>
+            <th>盈亏比率</th>
+            <th>1000元投资收益</th>
+          </thead>
+          <tbody>
+            <tr v-for="bean in data.trades">
+              <td>
+                <span v-if="bean.sellClosePoint > bean.buyClosePoint" class="label label-danger">盈利</span>
+                <span v-if="bean.sellClosePoint <= bean.buyClosePoint" class="label label-success">亏损</span>
+              </td>
+              <td>{{ bean.buyDate }}</td>
+              <td>{{ bean.buyClosePoint }}</td>
+              <td>{{ bean.sellDate }}</td>
+              <td>
+                <span v-if="bean.sellClosePoint == 0">n/a</span>
+                <span v-if="bean.sellClosePoint != 0">{{ bean.sellClosePoint }}</span>
+              </td>
+              <td>
+                <span v-if="bean.sellClosePoint == 0">n/a</span>
+                <span class="label"
+                  v-bind:class="{ 'label-danger': bean.sellClosePoint > bean.buyClosePoint, 'label-success': bean.sellClosePoint <= bean.buyClosePoint }"
+                  v-if="bean.sellClosePoint != 0">{{ (bean.sellClosePoint - bean.buyClosePoint) * 100 / bean.buyClosePoint |
+        formatNumberFilter(2) }}%</span>
+              </td>
+              <td>
+                <span v-if="bean.sellClosePoint == 0">n/a</span>
+                <span v-if="bean.sellClosePoint != 0">{{ bean.rate * 1000 | formatMoneyFilter }}</span>
+              </td>
+            </tr>
+          </tbody> -->
+        </table>
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -98,51 +174,58 @@ import { nextTick, ref, onMounted, reactive, onBeforeMount } from 'vue'
 import quantApi from '@/api/Quant/quantApi'
 const data = reactive({
   loading: true,
-  ma: 20,
-  buyThreshold: 1.01,
-  sellThreshold: 0.99,
-  startDate: null, // 开始时间
-  endDate: null, // 结束时间
   activeIncomeTab: 'first',
   activeProfitTab: 'first',
-  serviceCharge: 0.0,
-  // 1. 指数集合 由getCodes获取
-  indexes: [
+  // 1. 配置项
+  indexes: [ // 指数选择列表
     {
       code: '',
       name: ''
     }
   ],
-  // 2. 当前指数
-  currentIndex: '000300',
-  dates: [],
-  indexDatas: [], // api某指数数据
-  closePoints: [], // api某指数收盘价
-  // flushDate: true,
-  indexStartDate: null, // api某指数开始日期
-  indexEndDate: null, // api某指数结束日期
-  // 3. 收益数据
-  profits: [], // api
-  profitValues: [],
-  maDatas: [], // api
-  maValues: [],
-  // 4. 交易数据
-  trades: [], // api
-  // 5. 收益小结（6个收益api字段）
-  years: 0, // api
-  indexIncomeTotal: 0, // api
-  indexIncomeAnnual: 0, // api
-  trendIncomeTotal: 0, // api
-  trendIncomeAnnual: 0, // api
-  annualProfits: [], // api
-  annuals: [], // 年收益计算
-  indexIncomes: [], // 收益数组
-  trendIncomes: [], // 收益数组
-  // 6. 交易统计（4个交易统计api字段）
-  winCount: 0,
-  lossCount: 0,
-  avgWinRate: 0,
-  avgLossRate: 0
+  tacticsList: [{ // 投资类型选择列表
+    id: 0,
+    name: '趋势投资'
+  }],
+  currentIndex: '000300', // 表单-选中的指数
+  indexStartDate: null, // 当前指数的开始日期
+  indexEndDate: null, // 当前指数的结束日期
+  ma: 20, // 表单-均线
+  serviceCharge: 0.0, // 表单-手续费
+  buyThreshold: 1.01, // 表单-购买阈值
+  sellThreshold: 0.99, // 表单-出售阈值
+  startDate: null, // 表单-开始时间
+  endDate: null, // 表单-结束时间
+  // 2. 指数+均线+本身收益数据（3个api数组，4个计算）
+  indexDatas: [], // api某指数数据（对应的date日期、closePoints收盘价）
+  maDatas: [], // api某指数均线数据
+  profits: [], // api指数本身的日收益数据
+  years: 0, // 持续年时长
+  dates: [], // 计算的日期数组
+  closePoints: [], // 计算的指数收盘价
+  profitValues: [], // 计算的指数本身日收益数组（用于图表显示）
+  maValues: [], // 计算的指数均线值（用于图表显示）
+  // 3. 交易数据（1个api数组，4个api字段+2个计算）
+  trades: [], // api交易数组
+  tradeStastics: [{
+    winCount: 0,
+    lossCount: 0,
+    avgWinRate: 0,
+    avgLossRate: 0,
+    totalTradeCount: 0, // 总交易次数：winCount+lossCount
+    winRate: 0 // 胜率：((winCount/(winCount+lossCount))*100).toFixed(2)
+  }],
+  // 4. 收益数据（1个api数组，4个api字段，4计算）
+  annualProfits: [], // api多类目年收益数组（年份year、指数收益indexIncome、趋势投资收益trendIncome）
+  incomeStastics: [{
+    indexIncomeTotal: 0, // api指数收益率数值
+    indexIncomeAnnual: 0, // api指数年化收益率
+    trendIncomeTotal: 0, // api趋势投资收益率数值
+    trendIncomeAnnual: 0 // api趋势投资年化收益率
+  }],
+  annuals: [], // 计算的年份数组
+  indexIncomes: [], // 计算的年指数收益数组
+  trendIncomes: [] // 计算的年趋势收益数组收益数组
 })
 // let chart4Profit = null;
 // let chart4AnnualIncome = null;
@@ -162,12 +245,107 @@ const simulate = async () => {
     quantApi.simulateTest(params).then((res) => {
       console.log('res', res)
       if (res.status === 200) {
-        data.indexes = res.data
+        data.indexStartDate = indexStartDate;
+        data.indexEndDate = indexEndDate;
+        data.years = years
+        clearIndexDatasAndMADatas()
+        getIndexDatasAndMADatas(res.data.indexDatas, res.data.profits, res.data.maDatas)
+        clearTradesDatas()
+        getTradesDatas(res.data.trades, res.data.winCount, res.data.lossCount, res.data.avgWinRate, res.data.avgLossRate)
+
+        data.annualProfits = [];
+        data.annuals = [];
+        data.indexIncomes = [];
+        data.trendIncomes = [];
+
+        // 某种策略的收益数据（2个api数组+4个api数值）
+
+        data.annualProfits = res.data.annualProfits;
+        data.tacticsResults.profitStastics = []
+        const item1 = {
+          indexIncomeTotal: res.data.indexIncomeTotal,
+          indexIncomeAnnual: res.data.indexIncomeAnnual,
+          trendIncomeTotal: res.data.trendIncomeTotal,
+          trendIncomeAnnual: res.data.trendIncomeAnnual
+        }
+        data.incomeStastics.push(item1)
       }
       data.loading = false
     })
   } catch (error) {
     console.error('Failed to simulate:', error)
+  }
+}
+/**
+ * 清空指数数据(日期、收盘价)、均线数据、均线数组、指数数据、指数收益数组
+ */
+function clearIndexDatasAndMADatas () {
+  data.indexDatas = []
+  data.maDatas = []
+  data.profits = []
+  data.dates = []
+  data.closePoints = []
+  data.maValues = []
+  data.profitValues = []
+}
+/**
+ * 清空交易数据
+ */
+function clearTradesDatas () {
+  data.trades = [];
+  data.tradeStastics = []
+}
+/** * 获取[交易数据、盈利交易次数、亏损交易次数、平均盈利比率、平均亏损比率] ->[总交易次数、胜率]
+ * @param trades 交易数组
+ * @param winCount
+ * @param lossCount
+ * @param avgWinRate
+ * @param avgLossRate
+ * @returns trades、tradeStastics
+ */
+function getTradesDatas (trades, winCount, lossCount, avgWinRate, avgLossRate) {
+  try {
+    data.trades = trades
+    const item = {
+      winCount: winCount,
+      lossCount: lossCount,
+      avgWinRate: avgWinRate,
+      avgLossRate: avgLossRate,
+      totalTradeCount: winCount + lossCount,
+      winRate: ((winCount / (winCount + lossCount)) * 100).toFixed(2)
+    }
+    data.tradeStastics.push(item)
+  } catch (error) {
+    console.error('Failed to getTradesDatas:', error)
+  }
+}
+/** * 获取[指数数据、均线数据、起始日期、持续年]->[日期、收盘价、均线数组、指数收益数组]
+ * @param indexDatas
+ * @param profits
+ * @param maDatas
+ * @returns indexDatas、profits、maDatas；dates、maValues、profitValues
+ */
+function getIndexDatasAndMADatas (indexDatas, profits, maDatas) {
+  try {
+    data.indexDatas = indexDatas;
+    data.maDatas = maDatas;
+    data.profits = profits;
+    // 确保数据长度一致
+    if (data.indexDatas.length !== data.profits.length || data.indexDatas.length !== data.maDatas.length) {
+      throw new Error('Data lengths do not match');
+    }
+    for (let i = 0; i < data.indexDatas.length; i++) {
+      const indexData = data.indexDatas[i];
+      data.dates.push(indexData.date);
+      data.closePoints.push(indexData.closePoint);
+      const profit = data.profits[i];
+      data.profitValues.push(profit.value);
+      const ma = data.maDatas[i];
+      data.maValues.push(ma.value);
+    }
+    console.log('获取指数数据和均线数据成功')
+  } catch (error) {
+    console.error('Failed to get indexDatasAndMADatas:', error)
   }
 }
 const fetchIndexes = async () => {
@@ -179,9 +357,6 @@ const fetchIndexes = async () => {
         data.indexes = [...res.data]
         console.log('data.indexes', data.indexes)
         console.log('data.indexes', data.indexes[1].name)
-        // nextTick(() => {
-        //   simulate();
-        // });
       }
     })
   } catch (error) {
@@ -189,11 +364,6 @@ const fetchIndexes = async () => {
   }
 }
 const onSearch = () => {
-  simulate();
-}
-const changeParamWithFlushDate = (item) => {
-  data.startDate = null;
-  data.endDate = null;
   simulate();
 }
 
