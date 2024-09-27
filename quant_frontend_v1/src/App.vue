ec4f25f2ff48a236e8f2d49f56d23422f76bc869
Juemuel <799525644@qq.com>
2024-09-27 15:24:10 +0800

    增加窗口监听防抖

--- quant_frontend_v1/src/App.vue
+++ quant_frontend_v1/src/App.vue
@@ -8,7 +8,28 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup () {
+    // 1. 增加窗口监听防抖
+    const debounce = (fn, delay) => {
+      let timer
+      return (...args) => {
+        if (timer) {
+          clearTimeout(timer)
+        }
+        timer = setTimeout(() => {
+          fn(...args)
+        }, delay)
+      }
+    }
+    const _ResizeObserver = window.ResizeObserver;
+    window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+      constructor (callback) {
+        callback = debounce(callback, 200);
+        super(callback);
+      }
+    }
+  }
 })
 </script>
 
